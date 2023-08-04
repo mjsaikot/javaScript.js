5205dad8d84b4ed1ce11a73574ca0913c6756441
mjsaikot <mjsaikot@gmail.com>
2023-08-04 23:32:54 +0600

    Update file

---
+++ Basic/Output.js
@@ -0,0 +1,13 @@
+newFunction();
+
+function newFunction() {
+    let x = 5;
+    let y = 6;
+    if (x > y) {
+        console.log(y);
+    }
+    else {
+        console.log(x);
+    }
+    console.log("Note: This is my first javascript code");
+}
