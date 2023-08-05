99868bac54e4c48ef1ac99b8806173c3e832ee84
mjsaikot <mjsaikot@gmail.com>
2023-08-06 00:53:36 +0600

    update

---
+++ project/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.slide').owlCarousel
+    ({
+        items : 1,
+        nav:true,
+        center:true,
+        loop:true,
+        navText: ["&lt;", "&gt"],
+        autoplay:true,
+        animateIn:'bounceInRight',
+        animateOut:'bounceOutLeft',
+        autoplayTimeout:3000,
+        autoplaySpeed:1800,
+        margin:50,
+    });
+})
